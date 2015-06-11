d2868e3d93a657e5f9338c814b56fea9ebf2c0d5
Bastien Dronneau <basti1.dr@gmail.com>
2015-06-11 13:51:46 +0200

    Implement lib for consul and send to dashboard

--- lib/consul.rb
+++ lib/consul.rb
@@ -1,5 +1,5 @@
-# http://ruby-doc.org/stdlib-2.0.0/libdoc/open-uri/rdoc/OpenURI.html
-require 'open-uri'
+# https://github.com/rest-client/rest-client
+require 'rest-client'
 # https://github.com/flori/json
 require 'json'
 # http://stackoverflow.com/questions/9008847/what-is-difference-between-p-and-pp
@@ -7,8 +7,17 @@ require 'pp'
 
 class ConsulInfo
   def initialize
-    open("https://www.ruby-lang.org/") {|f|
-      f.each_line {|line| pp line}
-    }
+    @conf = ConfigApp.new()
+  end
+
+  def getAllCritical
+    errors = Array.new
+    @conf.params['consul']['servers'].each do |server|
+      response = JSON.parse(RestClient.get "#{server}/v1/health/state/critical")
+      if response.any?
+        errors.push(response)
+      end
+    end
+    errors
   end
 end
