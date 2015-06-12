c569bbcb2c3fccdbc3f20db5bc72ccc3122a36b1
Bastien Dronneau <basti1.dr@gmail.com>
2015-06-12 12:27:23 +0200

    Add NodeJS for parse Javascript and files rules

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+// gulp
+var gulp = require('gulp');
+
+// plugins
+var jshint = require('gulp-jshint');
+var jscs = require('gulp-jscs');
+var argv   = require('yargs').argv;
+var gulpIgnore = require('gulp-ignore');
+
+var filesJSToScan =  (argv.js ? argv.js : 'src/**/*.js');
+var filesJSToIgnore = [
+    '**/*jquery*',
+    '**/*min*',
+    '**/wowslider*',
+    '**/wowSlider/engine1/script.js',
+    '**/js/bootstrap.js',
+    '**/js/wowSliderForm.js'
+];
+
+gulp.task('jscs', function () {
+    return gulp.src(filesJSToScan)
+        .pipe(gulpIgnore.exclude(filesJSToIgnore))
+        .pipe(jscs());
+});
+
+gulp.task('jshint', function () {
+    gulp.src(filesJSToScan)
+        .pipe(gulpIgnore.exclude(filesJSToIgnore))
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+gulp.task('default',
+    ['jshint', 'jscs']
+);
