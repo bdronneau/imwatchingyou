934d935271f25aa1c2d450deba0c6596a4040cf9
Bastien Dronneau <basti1.dr@gmail.com>
2015-06-15 10:34:47 +0200

    Implement random_monitor, split contruction of graph point and random graph

--- lib/uptimerobot.rb
+++ lib/uptimerobot.rb
@@ -36,6 +36,33 @@ class UptimeRobot
   end
 
   def graph_data(monitor)
+    points = []
 
+    last_x = 1
+    total_value = 0
+
+    monitor['responsetime'].reverse.each do |value|
+      points.push({ x: last_x, y: value['value'].to_i })
+
+      total_value += value['value'].to_i
+      last_x += 1
+    end
+
+    points.push({ x: last_x + 1, y: total_value / last_x })
+  end
+
+  def random_monitor
+    data = []
+
+    monitors = all_monitors
+
+    monitor_random = monitors.sample
+
+    points = graph_data(monitor_random)
+
+    data.push(points)
+    data.push(monitor_random['friendlyname'])
+
+    data
   end
 end
