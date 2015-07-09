4d6e68c4a76adb820723467612f7b6a76b3c738d
Bastien Dronneau <basti1.dr@gmail.com>
2015-07-09 10:10:03 +0200

    Split class in method

--- lib/google_analytics.rb
+++ lib/google_analytics.rb
@@ -9,32 +9,17 @@ class GoogleAnalytics
   def initialize
     @conf = ConfigApp.new
 
-    service_account_email = @conf.params['google']['analytics']['service_account_email']
-    key_file = @conf.params['google']['analytics']['key_file']
-    key_secret = @conf.params['google']['analytics']['key_secret']
-    @profileID = @conf.params['google']['analytics']['profileID'].to_s
+    @profile_id = @conf.params['google']['analytics']['profileID'].to_s
 
-    @client = Google::APIClient.new(
-        :application_name => @conf.params['google']['analytics']['application_name'],
-        :application_version => @conf.params['google']['analytics']['application_version'])
-
-    ## Load our credentials for the service account
-    key = Google::APIClient::KeyUtils.load_from_pkcs12(key_file, key_secret)
-
-    @client.authorization = Signet::OAuth2::Client.new(
-        :token_credential_uri => 'https://accounts.google.com/o/oauth2/token',
-        :audience => 'https://accounts.google.com/o/oauth2/token',
-        :scope => 'https://www.googleapis.com/auth/analytics.readonly',
-        :issuer => service_account_email,
-        :signing_key => key)
-
-    ## Request a token for google analytics service account
-    @client.authorization.fetch_access_token!
+    # Init client
+    client
 
     @analytics = nil
-    ## Load cached discovered API, if it exists. This prevents retrieving the
-    ## discovery document on every run, saving a round-trip to the discovery service.
-    if File.exists? CACHED_API_FILE
+    # Load cached discovered API, if it exists.
+    # This prevents retrieving the
+    # discovery document on every run
+    # saving a round-trip to the discovery service.
+    if File.exist? CACHED_API_FILE
       File.open(CACHED_API_FILE) do |file|
         @analytics = Marshal.load(file)
       end
@@ -46,15 +31,50 @@ class GoogleAnalytics
     end
   end
 
-  def realtime_users
-    visitCount = @client.execute(
-        :api_method => @analytics.data.realtime.get,
-        :parameters => {
-            'ids' => "ga:" + @profileID,
-            'metrics' => "ga:activeVisitors",
-        })
+  def client
+    application_name = @conf.params['google']['analytics']['application_name']
+    application_version = @conf.params['google']['analytics']['application_version']
 
-    visitCount.data.rows[0][0]
+    @client = Google::APIClient.new(
+      application_name: application_name,
+      application_version: application_version
+    )
+
+    client_authorization
   end
 
+  def client_authorization
+    service_account_email = @conf.params['google']['analytics']['service_account_email']
+
+    @client.authorization = Signet::OAuth2::Client.new(
+      token_credential_uri: 'https://accounts.google.com/o/oauth2/token',
+      audience: 'https://accounts.google.com/o/oauth2/token',
+      scope: 'https://www.googleapis.com/auth/analytics.readonly',
+      issuer: service_account_email,
+      signing_key: load_key
+    )
+
+    ## Request a token for google analytics service account
+    @client.authorization.fetch_access_token!
+  end
+
+  def load_key
+    key_file = @conf.params['google']['analytics']['key_file']
+    key_secret = @conf.params['google']['analytics']['key_secret']
+
+    ## Load our credentials for the service account
+    Google::APIClient::KeyUtils.load_from_pkcs12(key_file, key_secret)
+  end
+
+  def realtime_users
+    visit_count = @client.execute(
+      api_method: @analytics.data.realtime.get,
+      parameters: {
+        'ids' =>     'ga:' + @profile_id,
+        'metrics' => 'ga:activeVisitors'
+      }
+    )
+
+    visit_count.data.rows[0][0]
+  end
 end
